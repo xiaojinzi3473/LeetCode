1bc0e24e8aa2ec4d771e6953e716ec847c792128
XiaoJinZi <41424687+xiaojinzi3473@users.noreply.github.com>
2019-06-20 12:35:24 +0800

    Create 142_Linked_list_Cycle_II.py

---
+++ 142_Linked_list_Cycle_II.py
@@ -0,0 +1,55 @@
+from proto.node_single import Node
+from utils.singly_linkedlist import SinglyLinkedListWithoutHead
+
+
+class Solution():
+    def detectCycle(self, head: Node):
+        d1 = dict()
+        cur = head
+        i = 0
+        while cur:
+            if cur in d1:
+                return(d1.get(cur))
+                # return cur
+            else:
+                d1[cur] = i
+            cur = cur.next
+            i = i + 1
+        return None
+
+class Solution2(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        d1 = dict()
+        while head:
+            if head in d1:
+                return head
+            else:
+                d1[head] = 1
+            head = head.next
+        return None
+
+if __name__ == '__main__':
+    #start_time = datetime.datetime.now()
+    #print(start_time)
+    l1 = SinglyLinkedListWithoutHead()
+    for data in range(4):
+        l1.insert_value_to_head(data)
+    l1.print_linkedList()
+    s1 = Solution()
+    print("Solution 1: ")
+    print(s1.detectCycle(l1.head))
+
+    # make tail linked to head
+    cur = l1.head
+    while cur.next:
+        cur = cur.next
+    cur.next = l1.head.next
+    #l1.print_linkedList()
+
+    print("\n---Has Cycle ---\n")
+    print("Solution 1: ")
+    print(s1.detectCycle(l1.head))
